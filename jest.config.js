a2c36908a9b5b737f1a8bc9609d890fe8d02eaaf
d0whc3r <d0whc3r@gmail.com>
2020-04-20 09:24:00 +0200

    test: update tests config

--- jest.config.js
+++ jest.config.js
@@ -1,27 +1,17 @@
 module.exports = {
-  moduleFileExtensions: [
-    'js',
-    'jsx',
-    'json',
-    'ts',
-    'tsx'
-  ],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
-  testMatch: [
-    '**/tests/**/*.spec.(js|jsx|ts|tsx)',
-    '**/__tests__/*.(js|jsx|ts|tsx)'
-  ],
+  testMatch: ['**/tests/**/*.spec.(js|jsx|ts|tsx)', '**/__tests__/*.(js|jsx|ts|tsx)'],
   testURL: 'http://localhost/',
   collectCoverage: true,
   coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.ts'
-  ],
+  collectCoverageFrom: ['src/**/*.ts'],
   cache: false,
   testResultsProcessor: 'jest-sonar-reporter',
   reporters: ['default', 'jest-junit'],
+  setupFiles: ['./jest.setup.js'],
   setupFilesAfterEnv: ['./jest.setup-after-env.js'],
   testEnvironment: 'node',
   globals: {
